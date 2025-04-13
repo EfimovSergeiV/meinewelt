afe698eb1e2f523824f1ccac49dd0eba1a5ec9a6
Sergei Efimov <efimovsergeiv@gmail.com>
2025-04-13 14:33:00 +0300

    add:

--- nuxt3-pwa/nuxt.config.ts
+++ nuxt3-pwa/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2025-04-13',
   devtools: { enabled: true },
 
   modules: [
@@ -33,4 +33,11 @@ export default defineNuxtConfig({
       type: "module"
     }
   },
+
+  css: [
+    '~/assets/css/tailwind.css',
+    '~/assets/css/main.css',
+    // '@mdi/font/css/materialdesignicons.min.css',
+  ],
+
 })
