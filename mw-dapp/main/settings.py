8e71e7419626f30c6dcec4e61a77066689c65682
S.Efimov <sys@tehnosvar.ru>
2025-04-24 14:37:00 +0300

    Edd:

--- mw-dapp/main/settings.py
+++ mw-dapp/main/settings.py
@@ -17,20 +17,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=z#1@z*qov2)o(rf)znqf-3kxxu3p(xkg!^1x&y71zi^*=88n*'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = [
-    "*",
-]
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -131,9 +117,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
