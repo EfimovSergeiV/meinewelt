9b1534747cff710bfc7b45e8b065cdd83fd29213
Sergei Efimov <efimovsergeiv@gmail.com>
2025-08-16 15:57:51 +0300

    rm: mw-nuxt (start ver.)

--- mw-nuxt/app.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <NuxtLayout>
-      <NuxtLoadingIndicator :color="'repeating-linear-gradient(to right,#0ea5e9 0% ,#075985 100%)'" :height="2"/>
-      <NuxtPage />
-    </NuxtLayout>
-  </div>
-</template>
--- mw-nuxt/pages/[404].vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="">
-    <div class="flex items-center justify-center">
-      <p class="uppercase font-semibold italic md:text-4xl">404 - Page not found</p>
-    </div>
-  </div>
-</template>
