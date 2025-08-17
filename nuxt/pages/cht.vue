7fc6db820e066a8443e8cc97e7f74a5da941bce6
Sergei Efimov <efimovsergeiv@gmail.com>
2025-08-17 21:12:25 +0300

    add:

--- nuxt/pages/cht.vue
+++ nuxt/pages/cht.vue
@@ -1,53 +1,24 @@
 <script setup>
 
-  const links = [
-    { id: 1, name: "Chat GPT", description: "AI сервис от OpenAI", img: "/GPT-tech.webp" },
-    { id: 2, name: "Chat GPT", description: "AI сервис от OpenAI", img: "/GPT-tech.webp" },
-    { id: 3, name: "Chat GPT", description: "AI сервис от OpenAI", img: "/GPT-tech.webp" },
-    { id: 4, name: "Chat GPT", description: "AI сервис от OpenAI", img: "/GPT-tech.webp" },
-    { id: 5, name: "Chat GPT", description: "AI сервис от OpenAI", img: "/GPT-tech.webp" },
-    { id: 6, name: "Chat GPT", description: "AI сервис от OpenAI", img: "/GPT-tech.webp" },
-    { id: 7, name: "Chat GPT", description: "AI сервис от OpenAI", img: "/GPT-tech.webp" },
-    { id: 8, name: "Chat GPT", description: "AI сервис от OpenAI", img: "/GPT-tech.webp" },
-    { id: 9, name: "Chat GPT", description: "AI сервис от OpenAI", img: "/GPT-tech.webp" },
-    { id: 10, name: "Chat GPT", description: "AI сервис от OpenAI", img: "/GPT-tech.webp" },
-    { id: 11, name: "Chat GPT", description: "AI сервис от OpenAI", img: "/GPT-tech.webp" },
-  ]
 
-
-
+  const messageText = ref("")
 
 </script>
 
 
 <template>
-  <div class="container mx-auto px-8">
+  <div class="container mx-auto px-4 py-8">
 
-    <!-- <div class="flex items-center justify-center h-60 text-gray-100 dark:text-gray-100">
-      <p class="text-2xl font-semibold">Ты бы дала за такой сайт?)))</p>
-    </div> -->
 
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  xl:grid-cols-5 gap-8">
-      <div v-for="link in links" :key="link.id" class="text-gray-100 dark:text-gray-100">
-        <div class="bg-cyan-900/70 border border-cyan-500/50 rounded-xl">
-          
-          <div class="">
-            <img :src="link.img" class="w-full rounded-t-xl" >
-            <div class="grid grid-cols-1 gap-1 px-2 py-2">
+    <div class="grid grid-cols-2 gap-4">
+      <div>
+        <p class="text-white">{{ messageText }}</p>
 
-              
-              <div class="">
-                <p class="text-xl">{{ link.id }}. {{ link.name }}</p>
-              </div>
-              <div class="">
-                <p class="text-base">{{ link.description }}</p>
-              </div>
-            </div>            
-          </div>
-
-          
-          
-        </div>
+      </div>      
+      
+      <div>
+        <textarea id="message" v-model="messageText" rows="4" class="px-4 py-2 w-full rounded-xl" placeholder="Напишите что-нибудь..."/>
+        <button class="mt-2 px-8 py-2 bg-blue-500 text-white rounded-lg" @click="firstButton">Отправить</button>
       </div>
 
 
