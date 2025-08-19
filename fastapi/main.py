28fbf28824d136a08feb83872fc67f31d0439f4b
EfimovSergeiV <efimovsergeiv@gmail.com>
2025-08-19 09:55:58 +0300

    Add:

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 html = """
 <!DOCTYPE html>
