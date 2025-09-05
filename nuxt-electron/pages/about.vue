82aaf54baaf0b00f2f7db9e3c2d3257dc110f95b
EfimovSergeiV <efimovsergeiv@gmail.com>
2025-09-05 11:18:51 +0300

    Add:

--- nuxt-electron/pages/about.vue
+++ nuxt-electron/pages/about.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="container mx-auto px-4 py-2">
 
-    <div>
+    <div class="flex gap-4 mt-8">
 
       <p class="text-white">О программе</p>
-      <p class="text-gray-300">nuxt 3.6.5</p>
-      <p class="text-gray-300">nuxt-electron 0.7.0</p>
-      <p class="text-gray-300">electron 29.3.0</p>
+      <div class="">
+        <p class="text-gray-300">nuxt 3.6.5</p>
+        <p class="text-gray-300">nuxt-electron 0.7.0</p>
+        <p class="text-gray-300">electron 29.3.0</p>        
+      </div>
+
 
     </div>
 
