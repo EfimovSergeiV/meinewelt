2ad16467e377bbe5bdb27c4e114ae5a4be83fa31
Sergei Efimov <efimovsergeiv@gmail.com>
2025-09-06 16:13:59 +0300

    add:

--- nuxt-electron/components/btn/DangerButton.vue
+++ nuxt-electron/components/btn/DangerButton.vue
@@ -12,7 +12,7 @@
     
     
     
-    <button class="bg-blue-700 active:bg-blue-800 border border-cyan-600 active:border-cyan-500 text-white w-36 px-6 py-2 rounded transition-all duration-100 select-none">
+    <button class="bg-red-500 active:bg-red-700 border border-rose-300 active:border-rose-200 text-white w-36 px-6 py-2 rounded transition-all duration-100 select-none">
       <p class="text-white">{{ text }}</p>
     </button>
 
--- nuxt-electron/pages/about.vue
+++ nuxt-electron/pages/about.vue
@@ -1,24 +1,36 @@
 <script setup lang="ts">
   const counter = useCounterStore()
+
+    import PrimaryButton from '~/components/btn/PrimaryButton.vue'
+    import DangerButton from '~/components/btn/DangerButton.vue'
+
 </script>
 
 <template>
   <div class="container mx-auto px-4 py-2">
 
-    <div class="flex gap-4 mt-8">
+    <div class="flex flex-wrap gap-8 mt-24">
 
-      <p class="text-white">О программе</p>
       <div class="">
-        <p class="text-gray-300">nuxt 3.6.5</p>
-        <p class="text-gray-300">nuxt-electron 0.7.0</p>
-        <p class="text-gray-300">electron 29.3.0</p>        
+        <p class="text-white">О программе</p>
+        <div class="py-8 px-4">
+          <p class="text-gray-300 hover:text-cyan-500">nuxt 3.6.5</p>
+          <p class="text-gray-300 hover:text-cyan-500">nuxt-electron 0.7.0</p>
+          <p class="text-gray-300 hover:text-cyan-500">electron 29.3.0</p>        
+        </div>
       </div>
 
-      <p class="text-white">Count: {{ counter.count }}</p>
       <div class="">
-        <button class="bg-blue-500/60 text-white px-4 py-2 rounded mr-2" @click="counter.increment">Increment</button>
-        <button class="bg-red-500/60 text-white px-4 py-2 rounded" @click="counter.decrement">Decrement</button>      
+        <p class="text-white">Count: {{ counter.count }}</p>
+        <div class="py-8 px-4 flex gap-4">
+          <PrimaryButton :text="`Добавить`" @click="counter.increment" />
+          <DangerButton :text="`Убрать`" @click="counter.decrement" />     
+        </div>
       </div>
+
+    
+    
+    
     </div>
 
 
