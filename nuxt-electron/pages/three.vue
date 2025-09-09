eafe82c413d8773eba40087cb485824e06ba4e97
EfimovSergeiV <efimovsergeiv@gmail.com>
2025-09-09 11:21:38 +0300

    Add:

--- nuxt-electron/pages/three.vue
+++ nuxt-electron/pages/three.vue
@@ -8,6 +8,8 @@
 
 <template>
   <div class="">
+    WARN  Module @tresjs/nuxt is disabled due to incompatibility issues:                                                                   11:19:46
+    - [nuxt] Nuxt version >=3.16.0 is required but currently using 3.6.5
 
 
     <ThreeExample />
